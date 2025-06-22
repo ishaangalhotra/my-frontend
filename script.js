c75dc68b86bff655f4a7e74992d0028352487d19
Ishan Galhotra <ishaangalhotra@gmail.com>
2025-06-23 00:53:55 +0530

    Connected frontend to backend product API

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 const API_BASE = "https://ecommerce-backend-8ykq.onrender.com";
+
+async function loadProducts() {
+  const res = await fetch(`${API_BASE}/api/products`);
+  const data = await res.json();
+
+  const productList = document.getElementById('product-list');
+  productList.innerHTML = "";
+
+  data.forEach(product => {
+    const div = document.createElement('div');
+    div.innerHTML = `
+      <h3>${product.name}</h3>
+      <p>${product.description}</p>
+      <strong>Price: ₹${product.price}</strong>
+      <hr>
+    `;
+    productList.appendChild(div);
+  });
+}
+
+window.onload = loadProducts;
